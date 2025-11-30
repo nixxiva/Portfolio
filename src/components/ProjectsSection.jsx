ccb367a1f56cebd7669917cf4690ebb79b4e40fa
Nix <sarahbustinera24@gmail.com>
2025-11-30 09:18:36 +0800

    Add project section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,10 +1,80 @@
+import { ExternalLink, Github } from "lucide-react";
 
 const projects = [
 	{
-		
+		id: 1,
+		title: "AI Meal Planner",
+		description: "AI Meal Planner – An API‑only Ruby on Rails backend paired with a React frontend that delivers AI‑powered, personalized meal planning.",
+		image: "/projects/project1.jpg",
+		tags: ["React", "Ruby on Rails", "PostgreSQL"],
+		demoUrl: "#",
+		githubUrl: "https://github.com/nixxiva/AI_Meal_Planning_App"
 	}
 ]
 
 export const ProjectsSection = () => {
-    return <section></section>;
-};
+    return (
+		<section id="projects" className="py-24 px-4 relative">
+			<div className="container mx-auto max-w-5xl">
+				<h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> 
+					Featured <span className="text-primary"> Projects </span>
+				</h2>
+
+				<p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+					Below are some of my projects using JavaScript, React, and Ruby on Rails, combining a modern frontend with a powerful backend. These projects gave me hands‑on experience in full‑stack development, from creating interactive user interfaces to designing RESTful APIs and managing databases using PostgreSQL.
+				</p>
+
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+					{projects.map((project, key) => (
+						<div 
+							key={key} 
+							className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+						>
+
+							<div className="h-48 overflow-hidden">
+								<img 
+									src={project.image} 
+									alt={project.title} 
+									className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" 
+								/>
+							</div>
+
+							<div className="p-6">
+								<div className="flex flex-wrap gap-2 mb-4">
+									{project.tags.map((tag) => (
+										<span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+											{tag}
+										</span>
+									))}
+								</div>
+							</div>
+
+							<h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+							<p className="text-muted-foreground text-xs mb-4">
+								{project.description}
+							</p>
+							<div className="flex justify-between items-center">
+								<div className="flex space-x-3">
+									<a 
+										href={project.demoUrl}
+										target="_blank" 
+										className="text-foreground/80 hover:text-primary transition-colors duration-300"
+									>
+										<ExternalLink size={20}/>
+									</a>
+									<a 
+										href={project.githubUrl}
+										target="_blank" 
+										className="text-foreground/80 hover:text-primary transition-colors duration-300"
+									>
+										<Github size={20}/>
+									</a>
+
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+)};
